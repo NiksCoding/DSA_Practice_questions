10c4b76cbe2c44d5fe76d514baa9028c0e756a7a
NiksCoding <dhanjugupta0911@gmail.com>
2023-09-17 01:46:12 +0530

    Update

---
+++ src/DSA_questions/avgOfAnArray.java
@@ -0,0 +1,21 @@
+package DSA_questions;
+
+public class avgOfAnArray {
+    int[] arr = {2,3,2,5,3,6,8};
+    int sum =0;
+    int n =arr.length;
+    public void average(){
+        for (int i =0; i<n ;i++){
+            sum += arr[i];
+            System.out.println(sum);
+
+        }
+        double avg = sum/n;
+        System.out.println(avg);
+    }
+
+    public static void main(String[] args) {
+        avgOfAnArray obj = new avgOfAnArray();
+        obj.average();
+    }
+}
