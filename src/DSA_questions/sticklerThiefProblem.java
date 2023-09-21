a1bca29e8a2e7c0c27db1bf36b22d41254031c48
NiksCoding <dhanjugupta0911@gmail.com>
2023-09-21 22:04:59 +0530

    Update

--- src/DSA_questions/sticklerThiefProblem.java
+++ src/DSA_questions/sticklerThiefProblem.java
@@ -1,11 +1,17 @@
 package DSA_questions;
 
+import java.util.Scanner;
+
 public class sticklerThiefProblem {
-    int[] arr = {1,2,3,4,5,7,6,7,8,9,10};
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr = new int[n];
     int oddSum=0;
     int evenSum=0;
     public void maximumLoots(){
         for (int i=0 ;i<arr.length ; i++){
+            System.out.println("gold coins in  house number "+ (i+1) );
+            arr[i]=sc.nextInt();
             if(i%2==1){
                 evenSum= evenSum+arr[i];
             }
@@ -14,14 +20,15 @@ public class sticklerThiefProblem {
             }
         }
         if(oddSum>evenSum){
-            System.out.println("maximum loot with odd house = "+ oddSum);
+            System.out.println("maximum loot with odd house = "+ oddSum + " Gold Coins");
         }
         else{
-            System.out.println("maximum loot with even house = " + evenSum);
+            System.out.println("maximum loot with even house = " + evenSum + " Gold Coins" );
         }
     }
 
     public static void main(String[] args) {
+        System.out.println("how many houses are there ?" );
         sticklerThiefProblem obj = new sticklerThiefProblem();
         obj.maximumLoots();
     }
