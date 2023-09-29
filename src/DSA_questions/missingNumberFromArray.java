c00155ed0db3fd461b98b87ca18f02ee658205a8
NiksCoding <dhanjugupta0911@gmail.com>
2023-09-30 02:43:43 +0530

    Update

---
+++ src/DSA_questions/missingNumberFromArray.java
@@ -0,0 +1,39 @@
+package DSA_questions;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class missingNumberFromArray {
+
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int sum=0;
+    public HashSet<Integer> getNumbersFromUser(){
+        //create ArrayList list
+       ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i<n ; i++){
+            System.out.println("enter number : " + i+1);
+            list.add(sc.nextInt());
+        }
+        // Instantiate an object of HashSet
+        HashSet<Integer> set = new HashSet<>(list);
+        System.out.println("your unique set : " + set);
+
+
+        for (int k : set) {
+            //System.out.println(itr.next());
+            sum += k;
+        }
+        System.out.println("sum = " + sum);
+
+        return set;
+    }
+    public static void main(String[] args) {
+        System.out.println("How many numbers u want to write ?");
+        missingNumberFromArray obj = new missingNumberFromArray();
+        obj.getNumbersFromUser();
+        //obj.findMissingNumber();
+    }
+}
