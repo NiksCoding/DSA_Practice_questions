9997d13e02b3865181ccd0b60c8707b33df9d9d5
NiksCoding <dhanjugupta0911@gmail.com>
2023-09-30 21:18:47 +0530

    Update

--- src/DSA_questions/missingNumberFromArray.java
+++ src/DSA_questions/missingNumberFromArray.java
@@ -10,11 +10,12 @@ public class missingNumberFromArray {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int sum=0;
-    public void getNumbersFromUser(){
+
+    public void findMissingNumber(){
         //create ArrayList list
        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i<n ; i++){
-            System.out.println("enter number : " + i+1);
+            System.out.println("enter number : " + (i+1));
             list.add(sc.nextInt());
         }
         // Instantiate an object of HashSet
@@ -25,13 +26,24 @@ public class missingNumberFromArray {
             //System.out.println(itr.next());
             sum += k;
         }
-        System.out.println("sum = " + sum);
+        System.out.println("sum of all unique number = " + sum);
+        Integer[] array = new Integer[set.size()];
+        //Converting Set object to integer array
+        set.toArray(array);
+        int arrayLength = array.length +1 ;
+        //calculating sum for expected array
+        int expectedSum = (arrayLength)*(arrayLength+1)/2 ;
+        int missingNumber = (expectedSum) - (sum) ;
+        //Get the missing number
+        if (expectedSum!=sum){
+            System.out.println("missingNumber = " + missingNumber);
+        }
 
     }
     public static void main(String[] args) {
+        System.out.println("enter n natural number in sequence from 1 to n skipping any one number between them. Numbers can be repetitive ");
         System.out.println("How many numbers u want to write ?");
         missingNumberFromArray obj = new missingNumberFromArray();
-        obj.getNumbersFromUser();
-        //obj.findMissingNumber();
+        obj.findMissingNumber();
     }
 }
