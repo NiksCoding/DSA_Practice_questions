fc4e6b51783d3f8855661857ac02a05c43d800bf
NiksCoding <dhanjugupta0911@gmail.com>
2024-01-13 19:22:11 +0530

    Update

---
+++ src/DSA_questions/ReverseString.java
@@ -0,0 +1,27 @@
+package DSA_questions;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+        String s = "My name is Nikhil";
+        char[] ch = s.toCharArray();
+        String s2 ="";
+        System.out.println(s.length());
+
+        int i=0;
+        while(i<ch.length){
+            s2=ch[i]+s2;
+            i++;
+        }
+        System.out.println(s2);
+        //or using for loop with 2nd method
+        String s3="";
+        for(int j=ch.length-1; j>=0;j--){
+            s3 += ch[j];
+
+        }
+        System.out.println(s3);
+
+    }
+
+}
