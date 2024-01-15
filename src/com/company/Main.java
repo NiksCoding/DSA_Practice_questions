5df6c3a48fac63cfcea74162296e930c67a898b0
NiksCoding <dhanjugupta0911@gmail.com>
2024-01-15 16:23:47 +0530

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
+  //array rotation
+    int [] i = {1,2,3,4,5};
+    int len = i.length;
+    public  void rotaion(int c){
+        //c from user
+        for(int j=c; j<len; j++){
+            System.out.print(i[j]);
+        }
+        for (int k =0 ;k<c;k++){
+            System.out.print(i[k]);
+        }
+
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Main obj = new Main();
+        obj.rotaion(3);
     }
+
 }
